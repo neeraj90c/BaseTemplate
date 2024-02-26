0ae530f66982ec3a5b7c3a985fc8e9901ab2fb78
Sumit Gore <110602649+GoreSumit@users.noreply.github.com>
2024-02-26 23:47:09 +0530

    code comment

--- AngularClient/src/app/Ticket/admin-dashboard/admin-dashboard.component.ts
+++ AngularClient/src/app/Ticket/admin-dashboard/admin-dashboard.component.ts
@@ -165,100 +165,100 @@ this.readyPriorityWisePieChart(res.priorityWiseCount)
   }
 
 
-readyClientWiseChart(dashData:KeyValue[]): void {
-    am5.array.each(am5.registry.rootElements, function (root) {
-        if (root && root.dom && root.dom.id && root.dom.id === "ClientWiseChart") {
-            root.dispose();
-        }
-    });
+// readyClientWiseChart(dashData:KeyValue[]): void {
+//     am5.array.each(am5.registry.rootElements, function (root) {
+//         if (root && root.dom && root.dom.id && root.dom.id === "ClientWiseChart") {
+//             root.dispose();
+//         }
+//     });
 
-    // Client wise chart
-    am5.ready(function () {
-        // Create root element
-        const root = am5.Root.new("ClientWiseChart");
+//     // Client wise chart
+//     am5.ready(function () {
+//         // Create root element
+//         const root = am5.Root.new("ClientWiseChart");
 
-        // Set themes
-        root.setThemes([am5themes_Animated.new(root)]);
+//         // Set themes
+//         root.setThemes([am5themes_Animated.new(root)]);
 
-        // Create chart
-        const chart = root.container.children.push(am5xy.XYChart.new(root, {
-            panX: true,
-            panY: true,
-            wheelX: "panX",
-            wheelY: "zoomX",
-            pinchZoomX: true,
-            paddingLeft: 0,
-            paddingRight: 1,
-        }));
+//         // Create chart
+//         const chart = root.container.children.push(am5xy.XYChart.new(root, {
+//             panX: true,
+//             panY: true,
+//             wheelX: "panX",
+//             wheelY: "zoomX",
+//             pinchZoomX: true,
+//             paddingLeft: 0,
+//             paddingRight: 1,
+//         }));
 
-        // Add cursor
-        const cursor = chart.set("cursor", am5xy.XYCursor.new(root, {}));
-        cursor.lineY.set("visible", false);
+//         // Add cursor
+//         const cursor = chart.set("cursor", am5xy.XYCursor.new(root, {}));
+//         cursor.lineY.set("visible", false);
 
-        // Create axes
-        const xRenderer = am5xy.AxisRendererX.new(root, {
-            minGridDistance: 30,
-            minorGridEnabled: true,
-        });
+//         // Create axes
+//         const xRenderer = am5xy.AxisRendererX.new(root, {
+//             minGridDistance: 30,
+//             minorGridEnabled: true,
+//         });
 
-        xRenderer.labels.template.setAll({
-            rotation: -90,
-            centerY: am5.p50,
-            centerX: am5.p100,
-            paddingRight: 15,
-        });
+//         xRenderer.labels.template.setAll({
+//             rotation: -90,
+//             centerY: am5.p50,
+//             centerX: am5.p100,
+//             paddingRight: 15,
+//         });
 
-        xRenderer.grid.template.setAll({
-            location: 1,
-        });
+//         xRenderer.grid.template.setAll({
+//             location: 1,
+//         });
 
-        const xAxis = chart.xAxes.push(am5xy.CategoryAxis.new(root, {
-            maxDeviation: 0.3,
-            categoryField: "key",
-            renderer: xRenderer,
-            tooltip: am5.Tooltip.new(root, {}),
-        }));
+//         const xAxis = chart.xAxes.push(am5xy.CategoryAxis.new(root, {
+//             maxDeviation: 0.3,
+//             categoryField: "key",
+//             renderer: xRenderer,
+//             tooltip: am5.Tooltip.new(root, {}),
+//         }));
 
-        const yRenderer = am5xy.AxisRendererY.new(root, {
-            strokeOpacity: 0.1,
-        });
+//         const yRenderer = am5xy.AxisRendererY.new(root, {
+//             strokeOpacity: 0.1,
+//         });
 
-        const yAxis = chart.yAxes.push(am5xy.ValueAxis.new(root, {
-            maxDeviation: 0.3,
-            renderer: yRenderer,
-        }));
+//         const yAxis = chart.yAxes.push(am5xy.ValueAxis.new(root, {
+//             maxDeviation: 0.3,
+//             renderer: yRenderer,
+//         }));
 
-        // Create series
-        const series = chart.series.push(am5xy.ColumnSeries.new(root, {
-            name: "Series 1",
-            xAxis: xAxis,
-            yAxis: yAxis,
-            valueYField: "totalTickets",
-            sequencedInterpolation: true,
-            categoryXField: "key",
-            tooltip: am5.Tooltip.new(root, {
-                labelText: "{valueY}",
-            }),
-        }));
+//         // Create series
+//         const series = chart.series.push(am5xy.ColumnSeries.new(root, {
+//             name: "Series 1",
+//             xAxis: xAxis,
+//             yAxis: yAxis,
+//             valueYField: "totalTickets",
+//             sequencedInterpolation: true,
+//             categoryXField: "key",
+//             tooltip: am5.Tooltip.new(root, {
+//                 labelText: "{valueY}",
+//             }),
+//         }));
 
-        series.columns.template.setAll({ cornerRadiusTL: 5, cornerRadiusTR: 5, strokeOpacity: 0 });
-        series.columns.template.adapters.add("fill", function (fill, target) {
-            return chart.get("colors").getIndex(series.columns.indexOf(target));
-        });
+//         series.columns.template.setAll({ cornerRadiusTL: 5, cornerRadiusTR: 5, strokeOpacity: 0 });
+//         series.columns.template.adapters.add("fill", function (fill, target) {
+//             return chart.get("colors").getIndex(series.columns.indexOf(target));
+//         });
 
-        series.columns.template.adapters.add("stroke", function (stroke, target) {
-            return chart.get("colors").getIndex(series.columns.indexOf(target));
-        });
+//         series.columns.template.adapters.add("stroke", function (stroke, target) {
+//             return chart.get("colors").getIndex(series.columns.indexOf(target));
+//         });
 
-        // Set data
-        xAxis.data.setAll(dashData);
-        series.data.setAll(dashData);
+//         // Set data
+//         xAxis.data.setAll(dashData);
+//         series.data.setAll(dashData);
 
-        // Make stuff animate on load
-        series.appear(1000);
-        chart.appear(1000, 100);
-    });
-}
+//         // Make stuff animate on load
+//         series.appear(1000);
+//         chart.appear(1000, 100);
+//     });
+// }
 
 
 }
